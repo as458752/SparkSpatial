dca7374b822b44410b931c5f85a8157da10cabb9
as458752 <as458752@gmail.com>
2017-04-16 14:04:28 -0700

    Update1

--- src/main/java/spark/hotspot/App.java
+++ src/main/java/spark/hotspot/App.java
@@ -1,5 +1,7 @@
 package spark.hotspot;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -45,12 +47,12 @@ public class App {
 
 				if (longitude.length() > 6 && latitude.length() > 6) {
 					try {
-						double lon = Double.parseDouble(longitude.substring(0, 6));
-						double lat = Double.parseDouble(latitude.substring(0, 5));
+						double lon = Double.parseDouble(longitude);
+						double lat = Double.parseDouble(latitude);
 						// log1.info("input : " + line);
 						String[] strs = neighborhoods(date, lon, lat);
 						for (int i = 0; i < 27; i++) {
-							if(i!=13) output.add(strs[i]);
+							output.add(strs[i]);
 						}
 					} catch (java.lang.NumberFormatException e) {
 					}
@@ -79,10 +81,11 @@ public class App {
 					}
 
 				});
-		List<Tuple2<Integer, String>> first50 = swappedPair.sortByKey(false).take(100);
+		List<Tuple2<Integer, String>> first50 = swappedPair.sortByKey(false).take(50);
+		
 		JavaPairRDD<Integer, String> finalOut = sc.parallelizePairs(first50);
 		// Save the 50 hottest spot back out to a text file
-		finalOut.saveAsTextFile(args[1]);
+		finalOut.values().saveAsTextFile(args[1]);
 	}
 
 	private static String[] neighborhoods(int time, double lon, double lat) {
@@ -90,10 +93,12 @@ public class App {
 		for (int a = -1; a < 2; a++) {
 			for (int b = -1; b < 2; b++) {
 				for (int c = -1; c < 2; c++) {
+					DecimalFormat df = new DecimalFormat("####");
+					//df.setRoundingMode(RoundingMode.DOWN);
 					int t = time + a;
-					double lo = lon + b * 0.01 - 0.01;
-					double la = lat + c * 0.01;
-					String str = t + "@" + String.format("%.2f", lo) + "@" + String.format("%.2f", la);
+					double lo = (lon + b * 0.01) * 100;
+					double la = (lat + c * 0.01) * 100;
+					String str = df.format(la)+ "," + df.format(lo) + "," + t;
 					out[(a + 1) * 9 + (b + 1) * 3 + c + 1] = str;
 				}
 			}
